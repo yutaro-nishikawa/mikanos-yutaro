29c4defa8d65c9b91b4fd61d5219d6b14408396c
nishikawa yutaro <you@example.com>
2024-01-11 22:42:52 +0900

    osbook_day04d

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -92,19 +92,18 @@ extern "C" void KernelMain(const FrameBufferConfig &frame_buffer_config)
         break;
     }
 
-
     for (int x = 0; x < frame_buffer_config.horizontal_resolution; ++x)
     {
         for (int y = 0; y < frame_buffer_config.vertical_resolution; y++)
         {
-            pixel_writer->Write( x, y, {255, 255, 255});
+            pixel_writer->Write(x, y, {255, 255, 255});
         }
     }
     for (int x = 0; x < 200; ++x)
     {
         for (int y = 0; y < 100; ++y)
         {
-            pixel_writer->Write(100 + x, 100 + y, {0, 255, 0});
+            pixel_writer->Write(x, y, {0, 255, 0});
         }
     }
     while (1)
